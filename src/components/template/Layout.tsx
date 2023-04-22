4db70ed5d21794b4bb83ff8259cd7a6d1411ff67
Kaito Suzuki <snails8d@gmail.com>
2023-04-22 09:46:10 +0900

    add: card design

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -16,8 +16,8 @@ const Layout = ({ location, title, children }: Props) => {
   return (
     <div data-is-root-path={isRootPath}>
       <Header location={location} />
-      <main className="flex justify-center items-center bg-gray-700">
-        <div className="bg-gray-600 w-55  mx-auto pl-1/4 pr-1/4 md:flex-col text-white font-30">
+      <main className="flex justify-center items-center bg-gray-600">
+        <div className="bg-gray-700 w-55  mx-auto pl-1/4 pr-1/4 md:flex-col text-white font-30">
           <div className="p-32">
             {children}
           </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,27 +27,26 @@ const BlogIndex = ({ data, location }: PageProps<GatsbyTypes.Query>) => {
       {/* <Bio /> */}
       <h1 className="text-3xl font-bold underline">test tailwind</h1>
 
-      <p>テスト</p>
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
           const title = post?.frontmatter?.title || post?.fields?.slug
 
           return (
-            <li key={post?.fields?.slug}>
+            <li key={post?.fields?.slug} className="my-5  text-white">
               <article
-                className="post-list-item"
+                className="border border-gray-200 rounded-md shadow-sm p-4"
                 itemScope
                 itemType="http://schema.org/Article"
               >
                 <header>
-                  <h2>
+                  <h1 className="text-3xl font-bold text-sky-400">
                     <Link to={post?.fields?.slug || ''} itemProp="url">
                       <span itemProp="headline">{title}</span>
                     </Link>
-                  </h2>
+                  </h1>
                   <small>{post?.frontmatter?.date}</small>
                 </header>
-                <section>
+                <section className="mt-2">
                   <p
                     dangerouslySetInnerHTML={{
                       __html: post?.frontmatter?.description as string || post.excerpt as string,
