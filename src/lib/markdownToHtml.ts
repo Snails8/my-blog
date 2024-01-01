904b44b00a7680d4d56a588bfa536c023112896d
Kaito Suzuki <snails8d@gmail.com>
2024-01-01 12:12:05 +0900

    add: markdown改良

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,14 @@
-import { remark } from 'remark'
+import { remark } from 'remark' // v13以降は不要
 import html from 'remark-html'
+import gfm from 'remark-gfm'
+import parse from 'remark-parse'
+import { unified } from 'unified'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
+  const result = await unified()
+    .use(parse)
+    .use(html)
+    .use(gfm)
+    .process(markdown)
   return result.toString()
 }
